9449741b810e81f44483cd72ac02ee0a204b5cec
ANANT0908 <87381952+ANANT0908@users.noreply.github.com>
2021-09-04 17:05:44 +0530

    aaaa

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
-// import using require
+const Shape = require("./shape");
 
 // declare class
 
-
 // export class using module.exports
+
+class Circle extends Shape {
+  constructor() {
+    super();
+  }
+
+  calculateArea() {
+    console.log("circle calc area");
+  }
+}
+
+module.exports = Circle;
